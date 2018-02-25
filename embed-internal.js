3ecb519b77df9b957b0f1eb589d91d96c5a7b54c
kyleplo <31634240+kyleplo@users.noreply.github.com>
2018-02-25 14:09:40 -0500

    Add files via upload

---
+++ embed-internal.js
@@ -0,0 +1,20 @@
+var site = "https://kyleplo.github.io";
+var target = "https://kyleplo.com";
+window.addEventListener("load",function (){
+window.parent.postMessage({action: "setTitle",data: document.title}, target);
+var links = document.getElementsByTagName("a");
+for(var i = 0;i < links.length;i++){
+if(links[i].href.startsWith(site)){
+links[i].addEventListener("click",function (e){
+e.preventDefault();
+window.parent.postMessage({action: "pushState",data: e.target.href}, target);
+location.href = e.target.href;
+setTimeout(function (){document.body.innerHTML = "<a href='" + site + e.target.href + "'>Click here</a>"},3000)
+});
+}else{
+links[i].addEventListener("click",function (e){
+e.preventDefault();
+window.parent.postMessage({action: "external",data: e.target.href}, target);
+setTimeout(function (){document.body.innerHTML = "<a href='" + e.target.href + "'>Click here</a>"},3000)
+})
+}}})
