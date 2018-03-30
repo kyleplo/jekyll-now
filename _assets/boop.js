b1a21de3b219dbf5a5a4aee2506ef6fcf500a740
kyleplo <31634240+kyleplo@users.noreply.github.com>
2018-03-30 18:09:54 -0400

    *boop*

---
+++ _assets/boop.js
@@ -0,0 +1,21 @@
+document.addEventListener("load",function (){
+ var boop = new Image();
+ boop.src = "https://kyleplo.github.io/_assets/boop.png";
+ boop.id = "boop";
+ boop.style.position = "fixed";
+ boop.style.top = "50%";
+ boop.style.left = "50%";
+ boop.style.animation = "boop 3s infinite";
+ document.body.appendChild(boop);
+ boop.setAttribute("hidden","hidden");
+ var boop2 = new Audio();
+ boop2.src = "https://kyleplo.github.io/_assets/boop.wav";
+ if(localStorage.adfree === "-13"){
+  bop();
+ }
+}
+function bop(){
+ boop.removeAttribute("hidden");
+ boop2.play();
+ setTimeout(function (){boop.setAttribute("hidden","hidden");},3000);
+}
