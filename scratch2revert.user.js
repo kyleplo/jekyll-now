2d01f4ef7b56e56443561301f576163e85af0b15
kyleplo <31634240+kyleplo@users.noreply.github.com>
2019-01-03 17:22:43 -0500

    Create scratch2revert.user.js

---
+++ scratch2revert.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         Scratch 2.0 Revert
+// @namespace    https://kyleplo.com/
+// @version      0.1
+// @description  Change some things in Scratch 3.0 to make it more like 2.0.
+// @author       kyleplo
+// @match        https://scratch.mit.edu/projects/*
+// @grant        none
+// @updateURL    https://kyleplo.com/scratch2revert.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementsByClassName("gui_editor-wrapper_2DYcj")[0].style.order = "2";
+    document.getElementsByClassName("sprite-selector_sprite-selector_2KgCX")[0].style.order = "2";
+    // Code copied from Theme3 userscript - thanks @NitroCipher
+    var style = document.createElement('style');
+    var styleAppend;
+    var categories = ["null", "motion", "looks", "sounds", "events", "control", "sensing", "operators", "data", "list", "pen", "more"];
+        var colors = ["null", "null", "null", "null", "#ffab19", "#e6bf00", "null", "null", "null", "null", "null", "null"];
+    categories.forEach(styleColor);
+    style.innerHTML = `
+    .injectionDiv {
+     zoom: 0.9;
+     overflow: hidden!important
+    }
+    .blocklyWidgetDiv, .blocklyDropDownDiv {
+     zoom: 0.9;
+    }
+    text.blocklyText {
+     font-size: 18px;
+    }
+    .question_question-container_OuJwy {
+     padding: 0.1rem;
+    }
+    line,
+    g[data-shapes] > path.blocklyBlockBackground {
+        stroke: #000000;
+        stroke-opacity: 0.2;
+    }
+    .blocklyDropDownDiv,
+    div.goog-menuitem,
+    g[data-argument-type~="dropdown"] > .blocklyBlockBackground,
+    path[data-argument-type] {
+        fill: #000000;
+        fill-opacity: 0.1;
+        stroke: #000000;
+        stroke-opacity: 0.2;
+    }` + styleAppend;
+
+    document.body.appendChild(style);
+
+    function styleColor(item, index){
+        if (colors[index] !== "null"){
+            styleAppend = styleAppend + `g[data-category="`+item+`" i] > path.blocklyBlockBackground {fill: `+colors[index]+`;}`;
+            styleAppend = styleAppend + `.scratchCategoryMenu:nth-child(`+(item+1)+`) .scratchCategoryItemBubble {background-color: `+colors[index]+`!important}`;
+        }
+        if (index == 4){ //I have no idea why index 4 is broken
+            styleAppend = styleAppend + `g[data-category="`+item+`" i] > path.blocklyBlockBackground {fill: `+colors[4]+`;}`;
+            styleAppend = styleAppend + `.scratchCategoryMenu:nth-child(`+(item+1)+`) .scratchCategoryItemBubble {background-color: `+colors[4]+`!important}`;
+        }
+    }
+    // End copied code
+})();
